28933cab158cc65403e6b8771600694397be0b97
alam2025 <alam13100852@gmail.com>
2023-06-16 18:21:47 +0600

    specific chefs api create

--- index.js
+++ index.js
@@ -12,10 +12,24 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+//chefs api's
+
 app.get('/chefs',async(req,res)=>{
       res.send(chefs)
 
+})//
+
+app.get('/chefs/:id',async(req,res)=>{
+      const id = req.params.id;
+      const selected = chefs.find(chef=>chef.id == id);
+      if(selected){
+            res.send(selected)
+      }
+      else{
+            res.send('Not Found')
+      }
 })
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
